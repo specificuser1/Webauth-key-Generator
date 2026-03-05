fe465cd8bf4b0413e4b6debf07091d03314e2d88
specificuser1 <motorolag14rx@gmail.com>
2026-03-05 05:54:17 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+require("dotenv").config()
+
+const express = require("express")
+const axios = require("axios")
+const app = express()
+
+const cooldown = {}
+
+app.use(express.static("public"))
+
+app.get("/generate", async (req,res)=>{
+
+let ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress
+
+if(cooldown[ip] && Date.now() < cooldown[ip]){
+let time = Math.ceil((cooldown[ip]-Date.now())/1000/60)
+return res.json({error:`Wait ${time} minutes`})
+}
+
+try{
+
+let response = await axios.post("https://keyauth.win/api/1.2/",{
+type:"license",
+ownerid:process.env.OWNER_ID,
+name:process.env.APP_NAME
+})
+
+let key = response.data.key
+
+cooldown[ip] = Date.now() + 6*60*60*1000
+
+res.json({key:key})
+
+}catch{
+res.json({error:"Key generation failed"})
+}
+
+})
+
+app.listen(3000,()=>{
+console.log("Server running")
+})
